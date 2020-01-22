c2391a763e2fd6f30bfb2dc63439993fe3f3a1ea
Dx00 <39845197+Dx00@users.noreply.github.com>
2020-01-22 19:34:07 -0300

    Delete index.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.use('/api', routes);
-routes.use(express.static(__dirname + "/client"));
-
-
-module.exports = routes;
